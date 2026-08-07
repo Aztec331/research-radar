d397912e7669cb482c5e8c52948a9ddd49032dfa
Aztec331 <adityabuzz331@gmail.com>
2026-08-07 23:27:36 +0530

    chore: initialize React and FastAPI structure

---
+++ react-client/src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+
+  return (
+    <div className="bg-red-900 min-h-screen flex items-center justify-center bg-slate-900">
+
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind is working Behenchod!
+      </h1>
+      
+    </div>
+  );
+
+
+}
